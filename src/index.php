9ca08b53e8e13800e1feea5dfc8fc2356a6f4f63
mythjava <33082411+mythjava@users.noreply.github.com>
2025-02-15 11:37:48 +0800

    Add files via upload

---
+++ src/index.php
@@ -0,0 +1,9 @@
+<?php
+$client_ip = $_SERVER['REMOTE_ADDR'];
+$browser = $_SERVER['HTTP_USRE_AGENT'];
+$request_uri = $_SERVER['REQUEST_URI'];
+echo "Hello, World!<br>";
+echo "client_ip: $client_ip<br>";
+echo "browser: $browser<br>";
+echo "request_uri: $request_uri<br>";
+?>
